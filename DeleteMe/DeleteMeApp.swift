1129db9c8c7affdd8addf2a449e9fa6441850fe7
stevenTheDeve <steventhedeve@gmail.com>
2023-08-19 16:51:01 -0700

    Initial Commit

---
+++ DeleteMe/DeleteMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeleteMeApp.swift
+//  DeleteMe
+//
+//  Created by Estevan Hinojosa on 8/19/23.
+//
+
+import SwiftUI
+
+@main
+struct DeleteMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
